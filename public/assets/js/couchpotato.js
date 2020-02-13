d369377ff0157bee6e350a43fefd7446cda89840
Khine Wai <khine.wai@uconn.edu>
2020-02-12 20:41:17 -0500

    1st Push

--- public/assets/js/couchpotato.js
+++ public/assets/js/couchpotato.js
@@ -1,6 +1,6 @@
-var username;
+var email;
 var password;
-var newUsername;
+var newemail;
 var newPassword;
 var firstName;
 var lastName;
@@ -18,10 +18,37 @@ $("#login-submit").on("click", function(event) {
     event.preventDefault();
 
     var loginCredentials = {
-        username: $("#inputUsername").val().trim(),
+        email: $("#inputUsername").val().trim(),
         password: $("#inputPassword").val().trim()
     };
 
+    if (!userData.email || !userData.password) {
+        return;
+      }
+  
+      // If we have an email and password we run the loginUser function and clear the form
+      loginUser(userData.email, userData.password);
+      emailInput.val("");
+      passwordInput.val("");
+    });
+  
+    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(email, password) {
+      $.post("/api/login", {
+        email: email,
+        password: password
+      })
+        .then(function() {
+          window.location.replace("/members");
+          // If there's an error, log the error
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
+  
+});
+    
     console.log(loginCredentials);
 });
 
@@ -30,28 +57,57 @@ $("#submit-new-user").on("click", function(event) {
     event.preventDefault();
 
     var newUser = {
-        newUsername: $("#inputUsernameNew").val().trim(),
-        newPassword: $("#inputPasswordNew").val().trim(),
-        firstName: $("#inputFirstName").val().trim(),
-        lastName: $("#inputLastName").val().trim(),
-        address: $("#inputAddress").val().trim(),
-        addressTwo: $("#inputAddress2").val().trim(),
-        city: $("#inputCity").val().trim(),
-        state: $("#inputState").val().trim(),
-        zipCode: $("#inputZip").val().trim()
+        newEmail: $("#inputEmailnameNew").val().trim(),
+        newPassword: $("#inputPasswordNew").val().trim()
+        // firstName: $("#inputFirstName").val().trim(),
+        // lastName: $("#inputLastName").val().trim(),
+        // address: $("#inputAddress").val().trim(),
+        // addressTwo: $("#inputAddress2").val().trim(),
+        // city: $("#inputCity").val().trim(),
+        // state: $("#inputState").val().trim(),
+        // zipCode: $("#inputZip").val().trim()
+
+        if (!userData.email || !userData.password) {
+            return;
+          }
+          // If we have an email and password, run the signUpUser function
+          signUpUser(userData.email, userData.password);
+          emailInput.val("");
+          passwordInput.val("");
+        });
+      
+        // Does a post to the signup route. If successful, we are redirected to the members page
+        // Otherwise we log any errors
+        function signUpUser(email, password) {
+          $.post("/api/signup", {
+            email: email,
+            password: password
+          })
+            .then(function(data) {
+              window.location.replace("/members");
+              // If there's an error, handle it by throwing up a bootstrap alert
+            })
+            .catch(handleLoginErr);
+        }
+      
+        function handleLoginErr(err) {
+          $("#alert .msg").text(err.responseJSON);
+          $("#alert").fadeIn(500);
+        }
+      });
     };
 
     console.log(newUser);
 
     $("#add-success").show();
 
-    $("#inputUsernameNew").empty();
+    $("#inputnameNew").empty();
     $("#inputPasswordNew").empty();
-    $("#inputFirstName").empty();
-    $("#inputLastName").empty();
-    $("#inputAddress").empty();
-    $("#inputAddress2").empty();
-    $("#inputCity").empty();
-    $("#inputState").val("");
-    $("#inputZip").empty();
+    // $("#inputFirstName").empty();
+    // $("#inputLastName").empty();
+    // $("#inputAddress").empty();
+    // $("#inputAddress2").empty();
+    // $("#inputCity").empty();
+    // $("#inputState").val("");
+    // $("#inputZip").empty();
 });
