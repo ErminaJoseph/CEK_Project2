7a82b5f2384d3eeef1ad91ae1b735e500054d195
Khine Wai <khine.wai@uconn.edu>
2020-02-15 14:03:40 -0500

    users

--- public/assets/js/couchpotato.js
+++ public/assets/js/couchpotato.js
@@ -15,6 +15,7 @@ $("#welcome").text("Welcome " + firstName +"!");
 
 $("#add-success").hide();
 
+<<<<<<< HEAD
 $("#login-submit").on("click", function(event) {
 
     event.preventDefault();
@@ -32,13 +33,94 @@ $("#login-submit").on("click", function(event) {
       loginUser(userData.email, userData.password);
       emailInput.val("");
       passwordInput.val("");
+=======
+$("#login-submit").on("click", function (event) {
+
+  event.preventDefault();
+
+  var loginCredentials = {
+    email: $("#inputEmailname").val().trim(),
+    password: $("#inputPassword").val().trim()
+  };
+
+  if (!loginCredentials.email || !loginCredentialspassword) {
+    return;
+  }
+
+  // // If we have an email and password we run the loginUser function and clear the form
+  // loginUser(loginCredentials.email, loginCredentials.password);
+  // emailInput.val("");
+  // passwordInput.val("");
+});
+
+// loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+function loginUser(email, password, ) {
+  $.post("/api/login", {
+    email: email,
+    password: password
+  })
+    .then(function () {
+      window.location.replace("/members");
+      // If there's an error, log the error
+    })
+    .catch(function (err) {
+      console.log(err);
+>>>>>>> users
     });
-  
-    // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-    function loginUser(email, password) {
-      $.post("/api/login", {
-        email: email,
-        password: password
+}
+
+
+
+// console.log(loginCredentials);
+
+
+$("#submit-new-user").on("click", function (event) {
+
+  event.preventDefault();
+
+  var newUser = {
+    newEmail: $("#inputEmailnameNew").val().trim(),
+    newPassword: $("#inputPasswordNew").val().trim(),
+    firstName: $("#inputFirstName").val().trim(),
+    lastName: $("#inputLastName").val().trim(),
+    address: $("#inputAddress").val().trim(),
+    addressTwo: $("#inputAddress2").val().trim(),
+    city: $("#inputCity").val().trim(),
+    state: $("#inputState").val().trim(),
+    zipCode: $("#inputZip").val().trim()
+  };
+
+  console.log(newUser.newEmail)
+
+  if (!newUser.newEmail || !newUser.newPassword || !newUser.firstName || !newUser.lastName || !newUser.address || !newUser.addressTwo || !newUser.city || !newUser.state || !newUser.zipCode) {
+    return;
+  }
+
+  console.log('logging out new user;', newUser);
+
+  // If we have an email and password, run the signUpUser function
+  signUpUser(newUser.newEmail, newUser.newPassword, newUser.firstName, newUser.lastName, newUser.address, newUser.addressTwo, newUser.city, newUser.state, newUser.zipCode);
+  // emailInput.val("");
+  // passwordInput.val("");
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(email, password, firstName, lastName, address, addressTwo, city, state, zipCode) {
+    $.post("/api/signup", {
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      address: address,
+      addressTwo: addressTwo,
+      city: city,
+      state: state,
+      zipCode: zipCode
+    })
+      .done(function (data) {
+        // window.location.replace("/members");
+        console.log(data);
+        // If there's an error, handle it by throwing up a bootstrap alert
       })
         .then(function() {
           window.location.replace("/members");
@@ -113,6 +195,5 @@ $("#submit-new-user").on("click", function(event) {
     console.log(newUser);
 
 
-
-
+      });
 
